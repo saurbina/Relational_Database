248e1ae055e8fbabf8b30e1b07def77fcd8d6a5e
Sebastián Urbina <60976810+saurbina@users.noreply.github.com>
2025-03-27 11:26:11 -0400

    Update salon_appointment_scheduler.sh

--- salon_appointment_scheduler/salon_appointment_scheduler.sh
+++ salon_appointment_scheduler/salon_appointment_scheduler.sh
@@ -10,8 +10,13 @@ MAIN_MENU() {
     echo -e "\n$1"
   fi
 
-  echo "Welcome to My Salon, how can I help you?" 
-  echo -e "\n1. cut\n2. color\n3. perm\n4. style\n5. trim"
+  AVAILABLE_SERVICES=$($PSQL "SELECT service_id, name FROM services")
+
+  echo -e "Welcome to My Salon, how can I help you?\n" 
+  echo "$AVAILABLE_SERVICES" | while read SERVICE_ID NAME
+  do
+    echo "$SERVICE_ID) $NAME" | sed -r 's/ \|//g'
+  done
   read SERVICE_ID_SELECTED
 
   # get service
@@ -42,22 +47,25 @@ MAIN_MENU() {
       CUSTOMER_ID=$($PSQL "SELECT customer_id FROM customers WHERE phone='$CUSTOMER_PHONE'")
       
       # ASK HOUR 
-      echo "What time would you like your cut, Fabio?"
+      echo -e "\nWhat time would you like your $SERVICE_NAME, $CUSTOMER_NAME?"
       read SERVICE_TIME
 
-      INSERT_HOUR_RESULT=$($PSQL "INSERT INTO customers(customer_id, service_id, time) VALUES($CUSTOMER_ID,$SERVICE_ID_SELECTED,'$SERVICE_TIME')") 
+      INSERT_HOUR_RESULT=$($PSQL "INSERT INTO appointments(customer_id, service_id, time) VALUES($CUSTOMER_ID,$SERVICE_ID_SELECTED,'$SERVICE_TIME')") 
       
-      MAIN_MENU "I have put you down for a $service_name at $SERVICE_TIME, $CUSTOMER_NAME."
+      echo "I have put you down for a $(echo $SERVICE_NAME | sed -r 's/^ *| *$//g') at $(echo $SERVICE_TIME | sed -r 's/^ *| *$//g'), $(echo $CUSTOMER_NAME | sed -r 's/^ *| *$//g')."
     else
       # get customer_id
       CUSTOMER_ID=$($PSQL "SELECT customer_id FROM customers WHERE phone='$CUSTOMER_PHONE'")
 
-      echo "What time would you like your $SERVICE_TIME, $CUSTOMER_NAME?"
+      echo -e "\nWhat time would you like your $(echo $SERVICE_NAME | sed -r 's/^ *| *$//g'), $(echo $name | sed -r 's/^ *| *$//g')?" 
       read SERVICE_TIME 
+      
+      INSERT_HOUR_RESULT=$($PSQL "INSERT INTO appointments(customer_id, service_id, time) VALUES($CUSTOMER_ID,$SERVICE_ID_SELECTED,'$SERVICE_TIME')") 
 
-      INSERT_HOUR_RESULT=$($PSQL "INSERT INTO customers(customer_id, service_id, time) VALUES($CUSTOMER_ID,$SERVICE_ID_SELECTED,'$SERVICE_TIME')") 
+      CLIENT_INFO=$($PSQL "SELECT * FROM appointments LEFT JOIN customers USING(customer_id)")
 
-      echo I have put you down for a $service_name at $SERVICE_TIME, $CUSTOMER_NAME.
+      echo "I have put you down for a $(echo $SERVICE_NAME | sed -r 's/^ *| *$//g') at $(echo $SERVICE_TIME | sed -r 's/^ *| *$//g'), $(echo $name | sed -r 's/^ *| *$//g')."
+
     fi
   fi  
 }
